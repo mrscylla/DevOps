e98ecdff9ac22adf434dca042fb858a94f2fd349
Aleksey.Marochkin <Aleksey.Marochkin@rtits.ru>
2019-01-30 11:23:45 +0300

    Исправления

--- cmd/admin1c_839.cmd
+++ cmd/admin1c_839.cmd
@@ -1,2 +1,2 @@
-regsvr32 "C:\Program Files\1cv8\8.3.9.2170\bin\radmin.dll" /s
+regsvr32 "C:\Program Files\1cv8\8.3.12.1616\bin\radmin.dll" /s
 start mmc "C:\Program Files\1cv8\common\1CV8 Servers (x86-64).msc"
