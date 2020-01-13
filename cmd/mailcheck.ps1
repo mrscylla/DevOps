649bc40f24b396267af79df863da3a5f2e82126a
Aleksey.Marochkin <Aleksey.Marochkin@rtits.ru>
2020-01-13 17:29:53 +0300

    Исправление load_changes.os

--- cmd/mailcheck.ps1
+++ cmd/mailcheck.ps1
@@ -1,8 +1,8 @@
 # Invoke-WebRequest -Uri http://1c-tasks.rtits.ru/check_mails?key=8c4Yl8xwYPzcDtY3QgaU
-Invoke-WebRequest -Uri https://1c-tasks.rtits.ru/admin/email_configurations/1/fetch?key=8c4Yl8xwYPzcDtY3QgaU
-Invoke-WebRequest -Uri https://1c-tasks.rtits.ru/admin/email_configurations/2/fetch?key=8c4Yl8xwYPzcDtY3QgaU
-Invoke-WebRequest -Uri https://1c-tasks.rtits.ru/admin/email_configurations/3/fetch?key=8c4Yl8xwYPzcDtY3QgaU
-Invoke-WebRequest -Uri https://1c-tasks.rtits.ru/admin/email_configurations/4/fetch?key=8c4Yl8xwYPzcDtY3QgaU
-Invoke-WebRequest -Uri https://1c-tasks.rtits.ru/admin/email_configurations/5/fetch?key=8c4Yl8xwYPzcDtY3QgaU
-Invoke-WebRequest -Uri https://1c-tasks.rtits.ru/admin/email_configurations/6/fetch?key=8c4Yl8xwYPzcDtY3QgaU
-Invoke-WebRequest -Uri https://1c-tasks.rtits.ru/admin/email_configurations/7/fetch?key=8c4Yl8xwYPzcDtY3QgaU
+Invoke-WebRequest -Uri http://1c-tasks.rtits.ru/admin/email_configurations/1/fetch?key=8c4Yl8xwYPzcDtY3QgaU
+Invoke-WebRequest -Uri http://1c-tasks.rtits.ru/admin/email_configurations/2/fetch?key=8c4Yl8xwYPzcDtY3QgaU
+Invoke-WebRequest -Uri http://1c-tasks.rtits.ru/admin/email_configurations/3/fetch?key=8c4Yl8xwYPzcDtY3QgaU
+Invoke-WebRequest -Uri http://1c-tasks.rtits.ru/admin/email_configurations/4/fetch?key=8c4Yl8xwYPzcDtY3QgaU
+Invoke-WebRequest -Uri http://1c-tasks.rtits.ru/admin/email_configurations/5/fetch?key=8c4Yl8xwYPzcDtY3QgaU
+Invoke-WebRequest -Uri http://1c-tasks.rtits.ru/admin/email_configurations/6/fetch?key=8c4Yl8xwYPzcDtY3QgaU
+Invoke-WebRequest -Uri http://1c-tasks.rtits.ru/admin/email_configurations/7/fetch?key=8c4Yl8xwYPzcDtY3QgaU
